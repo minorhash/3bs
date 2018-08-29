fe8f7bce015746d5cc6f393ed5edfb651a1c482c
minorhash <minorhash@gmail.com>
2018-08-29 11:34:07 +0900

    08/29/18

--- test/get.js
+++ test/get.js
@@ -1,7 +1,6 @@
-var url = 'http://localhost:3000';
+var url = 'http://localhost:3023';
 
 var app = require('../app'),
-ema = require('../routes/shop/son/ema'),
 chai = require('chai'),
 request = require('supertest'),
 age = require('superagent'),
@@ -10,13 +9,8 @@ expect = chai.expect;
 var ses = require('supertest-session');
 var testSes = null;
 
-var email=ema.AID
-var pss=ema.APSS
-
-var arr=[
-"shop","shop/cart"
-]
-
+var email="successful.payment@paidy.com"
+var pss="2112"
 
 describe('POST', function() {
 beforeEach(function () {
@@ -24,22 +18,12 @@ testSes = ses(app);
 });
 it('should sign in', function (done) {
     testSes.post("/shop")
-    .send({ email:email , pss:pss}) 
+    .send({ email:email , pss:pss})
     .expect(200)
 .end(function(err){
 if(err) return done(err)
 return done()
 })
 });
-
 })
-<<<<<<< HEAD
-=======
-done();
-          //console.log(res.req)
-});//end
-});//it
-}//for
-});//des
 
->>>>>>> cb3010d2fc20a996213c1119dc87ae1af29f9973

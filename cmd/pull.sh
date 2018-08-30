7ccfd68329c0685434706459757a41fba26adca0
minorhash <minorhash@gmail.com>
2018-08-30 16:12:59 +0900

    08/30/18

--- cmd/pull.sh
+++ cmd/pull.sh
@@ -1,8 +1,5 @@
-dir=$(pwd)
-str=${dir: -3}
-echo $str
-if [ -z $1 ];then
-    echo "usage"
-else
-git pull origin $1
-fi
+br=$(git rev-parse --abbrev-ref HEAD)
+echo $br
+
+git pull origin $br
+
--- cmd/push.sh
+++ cmd/push.sh
@@ -1,5 +1,5 @@
-if [ -z $1 ];then
-    echo "usage"
-else
-git push -u origin $1
-fi
+br=$(git rev-parse --abbrev-ref HEAD)
+echo $br
+
+git push -u origin $br
+

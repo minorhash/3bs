f40d479f3a6fc4a65764a94a9c939d0d9326b4ad
minorhash <minorhash@gmail.com>
2018-09-06 07:37:52 +0900

    09/06/18

--- cmd/pull.sh
+++ cmd/pull.sh
@@ -1,21 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> adr
 br=$(git rev-parse --abbrev-ref HEAD)
 echo $br
 
 git pull origin $br
-<<<<<<< HEAD
-=======
 if [ -z $1 ];then
     echo "usage"
 else
-git pull origin $1
+git pull origin
 fi
 
 
->>>>>>> nish
-=======
->>>>>>> adr
 
